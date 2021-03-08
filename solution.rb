4ed8fda823e2563c10a192173ca06025ba9ea4a2
Susana <susana.m.vik@gmail.com>
2021-03-08 09:47:25 -0500

    commit tree d022cc38e668bd3c22aa0d06be21a02bb1e6e73c

--- solution.rb
+++ solution.rb
@@ -1,7 +1,42 @@
 class Deck
+    attr_accessor :cards
 
+    @@all = []
+
+    def initialize(cards)
+        @cards = cards
+        @@all << self
+    end
+ 
+
+    def self.all
+        @@all
+    end
+
+    def choose_card
+        random_card = Deck.all.sample
+        random_card.remove
+    end
 end
 
 class Card
 
+    attr_reader :rank, :suit
+    rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    suit = ["Hearts", "Clubs", "Diamonds", "Spades"]
+
+    def initialize(rank, suit)
+        @rank = rank
+        @suit = suit
+    end  
+
+    def Deck.all
+        suits.each do |suit|
+            ranks.each do |rank|
+                Deck << Card.new(rank, suit)
+            end
+        end
+    end
 end
+
+# 5_of_spaces = Card.new(5, spades)
